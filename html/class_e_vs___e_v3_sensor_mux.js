813127cb42e45fb47dbad3cbfbc48eba256d5dbc
OpenElectrons <contact@openelectrons.com>
2016-04-20 15:28:20 -0400

    updating documentation

---
+++ html/class_e_vs___e_v3_sensor_mux.js
@@ -0,0 +1,8 @@
+var class_e_vs___e_v3_sensor_mux =
+[
+    [ "EVs_EV3SensorMux", "class_e_vs___e_v3_sensor_mux.html#acf62bd490da75ccbbbd8b1aa3bea96d6", null ],
+    [ "getMode", "class_e_vs___e_v3_sensor_mux.html#a9afc7aebd6a2bc31416ab0f4d98d85d2", null ],
+    [ "issueCommand", "class_e_vs___e_v3_sensor_mux.html#a78282e22024aea02f3f17df65d5d8253", null ],
+    [ "readValue", "class_e_vs___e_v3_sensor_mux.html#acea1491dc89259e4e2c0464fcec20cce", null ],
+    [ "setMode", "class_e_vs___e_v3_sensor_mux.html#a732dbd607116140511fff25daa80a64c", null ]
+];
