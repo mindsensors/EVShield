23069c62d942f3e054f961ed8f681f4ad6f36efb
Seth T <sethten@users.noreply.github.com>
2017-03-03 10:59:50 -0500

    EV3 infrared sensor test (works)
    Proximity works, couldn't test remote

--- EVShield_examples/MSBrowser/program1.ino
+++ EVShield_examples/MSBrowser/program1.ino
@@ -1,27 +1,27 @@
 #include <EVShield.h>
 #include <EVs_UIModule.h>
-#include <EVs_EV3Ultrasonic.h>
+#include <EVs_EV3Infrared.h>
 
-EVs_EV3Ultrasonic myUS;
+EVs_EV3Infrared myIR;
 
 void setup1() {
-    uim.println("EV3 ultrasonic sensor test");
-    myUS.init(&ev, SH_BAS1);
-    myUS.setMode(MODE_Sonar_Inches);
+    uim.println("EV3 infrafred sensor test");
+    myIR.init(&ev, SH_BAS1);
+    myIR.setMode(MODE_Infrared_Proximity);
     
     uim.setCursor(0, 4*16);
-    uim.print("distance: ");
+    uim.print("proximity: ");
 }
 
 void loop1() {
     if (ev.getButtonState(BTN_GO)) ESP.reset();
     
-    if (myUS.detect())
-        uim.writeLine(0, 3, "Device found", true, EVs_UIM_WHITE);
-    else
-        uim.writeLine(0, 3, "No device found", true, EVs_UIM_WHITE);
+    // if (myUS.detect())
+    //     uim.writeLine(0, 3, "Device found", true, EVs_UIM_WHITE);
+    // else
+    //     uim.writeLine(0, 3, "No device found", true, EVs_UIM_WHITE);
     
     uim.clearLine(6);
     uim.setCursor(0, 5*16);
-    uim.println(myUS.getDist());
+    uim.println(myIR.readProximity());
 }
