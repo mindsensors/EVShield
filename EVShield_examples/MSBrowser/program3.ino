b9923ae7ecb25fbb8e881e1dacb68c6f7579b3cf
Seth T <sethten@users.noreply.github.com>
2017-03-03 10:02:21 -0500

    Revert "Display AbsoluteIMU value with progress bar"

--- EVShield_examples/MSBrowser/program3.ino
+++ EVShield_examples/MSBrowser/program3.ino
@@ -4,32 +4,41 @@
 
 EVs_AbsoluteIMU imu;
 
-void drawBar(int height, int progress);
-
 void setup3() {
     imu.init(&ev, SH_BAS1);
     
     uim.println(imu.getFirmwareVersion());
     uim.println(imu.getDeviceID());
     uim.println(imu.getVendorID());
-    uim.fillRect(0, 16*4, 320, 16, EVs_UIM_WHITE);
-    uim.fillRect(0, 16*6, 320, 16, EVs_UIM_WHITE);
-    uim.fillRect(0, 16*8, 320, 16, EVs_UIM_WHITE);
+    uim.println();
+    uim.println("gyro, accel, magneticField");
 }
 
 void loop3() {
     if (ev.getButtonState(BTN_GO)) ESP.reset();
     
+    gyro mygyro;
+    imu.readGyro(mygyro);
+    uim.clearLine(6);
+    uim.setCursor(0, 16*5);
+    uim.printf("x:%d | y:%d | z:%d", mygyro.gx, mygyro.gy, mygyro.gz);
+    
     accl myaccl;
     imu.readAccelerometer(myaccl);
+    uim.clearLine(7);
+    uim.setCursor(0, 16*6);
+    uim.printf("x:%d | y:%d | z:%d", myaccl.tx, myaccl.ty, myaccl.tz);
     
-    drawBar(16*4+1, myaccl.tx);
-    drawBar(16*6+1, myaccl.ty);
-    drawBar(16*8+1, myaccl.tz);
-}
-
-void drawBar(int height, int progress) {
-    int val = 318 * progress / 256;
-    uim.fillRect(1, height, val, 14, EVs_UIM_GREEN);
-    uim.fillRect(val+1, height, 318-val, 14, EVs_UIM_BLACK);
+    magnetic_field mymgnt;
+    imu.readMagneticField(mymgnt);
+    uim.clearLine(8);
+    uim.setCursor(0, 16*7);
+    uim.printf("x:%d | y:%d | z:%d", mymgnt.mx, mymgnt.my, mymgnt.mz);
+    
+    cmps mycmps;
+    imu.readCompass(mycmps);
+    uim.clearLine(9);
+    uim.clearLine(10);
+    uim.setCursor(0, 16*8);
+    uim.printf("compass: %d\nerror: %d", mycmps.heading, mycmps.error);
 }
