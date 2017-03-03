bd20c10456171f73ca82b0074641688528f37f6e
Seth T <sethten@users.noreply.github.com>
2017-03-03 11:31:56 -0500

    Short motor test

--- EVShield_examples/MSBrowser/program1.ino
+++ EVShield_examples/MSBrowser/program1.ino
@@ -1,27 +1,18 @@
 #include <EVShield.h>
 #include <EVs_UIModule.h>
-#include <EVs_EV3Infrared.h>
-
-EVs_EV3Infrared myIR;
 
 void setup1() {
-    uim.println("EV3 infrafred sensor test");
-    myIR.init(&ev, SH_BAS1);
-    myIR.setMode(MODE_Infrared_Proximity);
-    
-    uim.setCursor(0, 4*16);
-    uim.print("proximity: ");
+    uim.println("Motor test");
+    ev.bank_a.motorRunRotations(SH_Motor_1, 
+                 SH_Direction_Forward, 
+                 SH_Speed_Medium,
+                 1, 
+                 SH_Completion_Wait_For,
+                 SH_Next_Action_BrakeHold);
+    uim.println("Done (spun one rotation)");
+    ESP.reset();
 }
 
 void loop1() {
     if (ev.getButtonState(BTN_GO)) ESP.reset();
-    
-    // if (myUS.detect())
-    //     uim.writeLine(0, 3, "Device found", true, EVs_UIM_WHITE);
-    // else
-    //     uim.writeLine(0, 3, "No device found", true, EVs_UIM_WHITE);
-    
-    uim.clearLine(6);
-    uim.setCursor(0, 5*16);
-    uim.println(myIR.readProximity());
 }
