2b646f7bd788f76af1a0851c95f3822384e586f2
Seth T <sethten@users.noreply.github.com>
2017-03-03 09:29:19 -0500

    Clean up print output of AbsoluteIMU example

--- EVShield_examples/MSBrowser/program3.ino
+++ EVShield_examples/MSBrowser/program3.ino
@@ -10,6 +10,8 @@ void setup3() {
     uim.println(imu.getFirmwareVersion());
     uim.println(imu.getDeviceID());
     uim.println(imu.getVendorID());
+    uim.println();
+    uim.println("gyro, accel, magneticField");
 }
 
 void loop3() {
@@ -17,29 +19,26 @@ void loop3() {
     
     gyro mygyro;
     imu.readGyro(mygyro);
-    uim.clearLine(5);
     uim.clearLine(6);
-    uim.setCursor(0, 16*4);
-    uim.printf("gyro x:%d | y:%d | z:%d", mygyro.gx, mygyro.gy, mygyro.gz);
+    uim.setCursor(0, 16*5);
+    uim.printf("x:%d | y:%d | z:%d", mygyro.gx, mygyro.gy, mygyro.gz);
     
     accl myaccl;
     imu.readAccelerometer(myaccl);
     uim.clearLine(7);
-    uim.clearLine(8);
     uim.setCursor(0, 16*6);
-    uim.printf("accl x:%d | y:%d | z:%d", myaccl.tx, myaccl.ty, myaccl.tz);
+    uim.printf("x:%d | y:%d | z:%d", myaccl.tx, myaccl.ty, myaccl.tz);
     
     magnetic_field mymgnt;
     imu.readMagneticField(mymgnt);
-    uim.clearLine(9);
-    uim.clearLine(10);
-    uim.setCursor(0, 16*8);
-    uim.printf("magn x:%d | y:%d | z:%d", mymgnt.mx, mymgnt.my, mymgnt.mz);
+    uim.clearLine(8);
+    uim.setCursor(0, 16*7);
+    uim.printf("x:%d | y:%d | z:%d", mymgnt.mx, mymgnt.my, mymgnt.mz);
     
     cmps mycmps;
     imu.readCompass(mycmps);
-    uim.clearLine(11);
-    uim.clearLine(12);
-    uim.setCursor(0, 16*10);
-    uim.printf("compass: %d\n%d", mycmps.heading, mycmps.error);
+    uim.clearLine(9);
+    uim.clearLine(10);
+    uim.setCursor(0, 16*8);
+    uim.printf("compass: %d\nerror: %d", mycmps.heading, mycmps.error);
 }
