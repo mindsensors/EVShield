26290962120e8a1b432469873268f2746569a9a2
Seth T <sethten@users.noreply.github.com>
2017-03-03 10:14:55 -0500

    NXT touch sensor test (works)

--- EVShield_examples/MSBrowser/program1.ino
+++ EVShield_examples/MSBrowser/program1.ino
@@ -1,32 +1,19 @@
 #include <EVShield.h>
 #include <EVs_UIModule.h>
+#include <EVs_NXTTouch.h>
+
+EVs_NXTTouch myTouch;
 
 void setup1() {
-    uim.println("Button tests");
+    uim.println("NXT touch sensor test");
+    myTouch.init(&ev, SH_BAS1);
 }
 
 void loop1() {
     if (ev.isTouched()) ESP.reset();
     
-    if (ev.getButtonState(BTN_GO))
-        uim.writeLine(0, 3, "GO is pressed", true, EVs_UIM_WHITE); // left edge, first line, text, do clear line, white text
+    if (myTouch.isPressed())
+        uim.writeLine(0, 3, "pressed", true, EVs_UIM_WHITE);
     else
-        uim.writeLine(0, 3, "GO is not pressed", true, EVs_UIM_WHITE);
-    
-    if (ev.getButtonState(BTN_LEFT))
-        uim.writeLine(0, 4, "Left is pressed", true, EVs_UIM_WHITE);
-    else
-        uim.writeLine(0, 4, "Left is not pressed", true, EVs_UIM_WHITE);
-    
-    if (ev.getButtonState(BTN_RIGHT))
-        uim.writeLine(0, 5, "Right is pressed", true, EVs_UIM_WHITE);
-    else
-        uim.writeLine(0, 5, "Right is not pressed", true, EVs_UIM_WHITE);
-    
-    uim.writeLine(0, 7, "EVs_UIModule\n  .getButtonState: ", false, EVs_UIM_WHITE);
-    uim.print(uim.getButtonState(EVs_BTN_LEFT));
-    uim.print(uim.getButtonState(EVs_BTN_RIGHT));
-    uim.print(uim.getButtonState(EVs_BTN_UP));
-    uim.print(uim.getButtonState(EVs_BTN_DOWN));
-    uim.print(uim.getButtonState(EVs_BTN_CLICK));
+        uim.writeLine(0, 3, "released", true, EVs_UIM_WHITE);
 }
