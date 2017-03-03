3ae1fd4b0284ee241e61ffbd76080bccec02cf74
Seth T <sethten@users.noreply.github.com>
2017-03-03 13:26:25 -0500

    EV3 infrared beacon test (works)

--- EVShield_examples/MSBrowser/program1.ino
+++ EVShield_examples/MSBrowser/program1.ino
@@ -7,10 +7,12 @@ EVs_EV3Infrared myIR;
 void setup1() {
     uim.println("EV3 infrafred sensor test");
     myIR.init(&ev, SH_BAS1);
-    myIR.setMode(MODE_Infrared_Remote);
+    myIR.setMode(MODE_Infrared_Beacon);
     
     uim.setCursor(0, 2*16);
-    uim.print("button: ");
+    uim.print("heading: ");
+    uim.setCursor(0, 5*16);
+    uim.print("proximity: ");
 }
 
 void loop1() {
@@ -18,5 +20,8 @@ void loop1() {
     
     uim.clearLine(4);
     uim.setCursor(0, 3*16);
-    uim.println(myIR.readChannelButton(1));
+    uim.println(myIR.readChannelHeading(1));
+    uim.clearLine(7);
+    uim.setCursor(0, 6*16);
+    uim.println(myIR.readChannelProximity(1));
 }
