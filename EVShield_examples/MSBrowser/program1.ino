c736b0dc87082f46436337aa57f87ffffaad6b68
Seth T <sethten@users.noreply.github.com>
2017-03-03 10:38:59 -0500

    EV3 ultrasonic sensor test (works)
    .detect() doesn't work the best, but it's the same case on the EVShield

--- EVShield_examples/MSBrowser/program1.ino
+++ EVShield_examples/MSBrowser/program1.ino
@@ -1,23 +1,27 @@
 #include <EVShield.h>
 #include <EVs_UIModule.h>
-#include <EVs_EV3Touch.h>
+#include <EVs_EV3Ultrasonic.h>
 
-EVs_EV3Touch myTouch;
+EVs_EV3Ultrasonic myUS;
 
 void setup1() {
-    uim.println("EV3 touch sensor test");
-    myTouch.init(&ev, SH_BAS1);
+    uim.println("EV3 ultrasonic sensor test");
+    myUS.init(&ev, SH_BAS1);
+    myUS.setMode(MODE_Sonar_Inches);
+    
+    uim.setCursor(0, 4*16);
+    uim.print("distance: ");
 }
 
 void loop1() {
-    if (ev.isTouched()) ESP.reset();
+    if (ev.getButtonState(BTN_GO)) ESP.reset();
     
-    if (myTouch.isPressed())
-        uim.writeLine(0, 3, "pressed", true, EVs_UIM_WHITE);
+    if (myUS.detect())
+        uim.writeLine(0, 3, "Device found", true, EVs_UIM_WHITE);
     else
-        uim.writeLine(0, 3, "released", true, EVs_UIM_WHITE);
+        uim.writeLine(0, 3, "No device found", true, EVs_UIM_WHITE);
     
-    uim.clearLine(4);
-    uim.setCursor(0, 3*16);
-    uim.printf("bump count: %u", myTouch.getBumpCount());
+    uim.clearLine(6);
+    uim.setCursor(0, 5*16);
+    uim.println(myUS.getDist());
 }
