90ee67bd63e3fdf1ff91d1047fac89b6e6112dae
Seth T <sethten@users.noreply.github.com>
2017-03-08 15:24:19 -0500

    Web server motor run

--- WiFiArduinoInterfaceForPiStorms_examples/WiFi_Examples/program1.ino
+++ WiFiArduinoInterfaceForPiStorms_examples/WiFi_Examples/program1.ino
@@ -3,15 +3,27 @@
 #include <EVShield.h>
 #include <EVs_UIModule.h>
 
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server(80);
+
+void handleRunMotorRequest() {
+    ev.bank_a.motorRunRotations(SH_Motor_1, SH_Direction_Forward, SH_Speed_Slow,
+                                3, SH_Completion_Dont_Wait, SH_Next_Action_Float);
+    server.send(200, "text/plain", "rotating...");
+}
+
 void setup1() {
-    // ev and uim have already been instantiated, you can just use them now.
-    ev.ledSetRGB(0, 0, 0); // ev = EVShield
-    uim.println("setup1"); // uim = UI Module
+    server.on("/", handleRunMotorRequest);
+    server.begin();
+    
+    uim.println("My IP address is:");
+    uim.println(WiFi.localIP());
 }
 
 void loop1() {
     // reset to browser when GO is pressed
     if (ev.getButtonState(BTN_GO)) ESP.reset();
     
-    // your creative code here
+    server.handleClient();
 }
