60f698a15df9312fe3d16f6a9547d8ed6fa615c6
Seth T <sethten@users.noreply.github.com>
2017-03-08 15:25:20 -0500

    Keep examples in separate folder

--- EVShield_examples/MSBrowser/credentials.h
+++
@@ -1,9 +0,0 @@
-#ifndef MS_CREDENTIALS
-#define MS_CREDENTIALS
-
-// used for uploading OTA (over the air)
-const char* SSID     = "your_WiFi_network";
-const char* PASSWORD = "letmein";
-const char* UPLOAD_PASSWORD = "";
-
-#endif
