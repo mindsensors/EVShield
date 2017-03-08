65ffbe497b5945c461c4ff3598f90805f0bef901
Seth T <sethten@users.noreply.github.com>
2017-03-08 15:24:37 -0500

    Archive other examples

--- EVShield_examples/MSBrowser/program1.ino
+++ EVShield_examples/MSBrowser/program1.ino
@@ -6,6 +6,8 @@
 void setup1() {
     // ev and uim have already been instantiated, you can just use them now.
     ev.ledSetRGB(0, 0, 0); // ev = EVShield
+    ev.bank_a.motorRunRotations(SH_Motor_1, SH_Direction_Forward, SH_Speed_Slow,
+                                2, SH_Completion_Wait_For, SH_Next_Action_Float);
     uim.println("setup1"); // uim = UI Module
 }
 
