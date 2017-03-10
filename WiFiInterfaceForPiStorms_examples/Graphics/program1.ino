0adba4f6fb36c77a0229f8ae01da84c8c5a69b72
Seth T <sethten@users.noreply.github.com>
2017-03-10 13:47:53 -0500

    Add copyright statements and more comments

--- WiFiInterfaceForPiStorms_examples/Graphics/program1.ino
+++ WiFiInterfaceForPiStorms_examples/Graphics/program1.ino
@@ -1,3 +1,26 @@
+/************************************************************************/
+/*                                                                 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+  History:
+  Date      Author          Comments
+  Mar 2017  Seth Tenembaum  initial
+
+************************************************************************/
+
+// this example will cover many uses of text and different colors
 // remember to update credentials.h so the device can connect to your network!
 
 #include <EVShield.h>
@@ -13,6 +36,7 @@ void setup1() {
     
     // x = 0 is the left side of the screen, 8 means the 8th line of text from the top, true is to clear anything that was there before
     uim.writeLine(0, 8, "If you want to print at a specific line number, use .writeLine", true, EVs_UIM_CYAN);
+    // this could be useful if you want to update the value on a line, as it can clear whatever was there before
     
     // this will be just below the writeLine text, and printed in cyan
     uim.println("Ta-da!");
@@ -33,7 +57,7 @@ void setup1() {
     
     // if not specified, the background color will default to black
     uim.setTextColor(EVs_UIM_YELLOW);
-    // besides .setTextColor, you can also move the cursor
+    // besides .setTextColor, you can also move the cursor by pixels
     uim.setCursor(260, 8);
     // and set text size
     uim.setTextSize(1);
@@ -48,5 +72,5 @@ void loop1() {
     // reset to browser when GO is pressed
     if (ev.getButtonState(BTN_GO)) ESP.reset();
     
-    // your creative code here
+    // no need to repeat anything in this example, just wait for GO to reset
 }
