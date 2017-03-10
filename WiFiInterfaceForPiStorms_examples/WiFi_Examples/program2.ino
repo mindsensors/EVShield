1f5321dfdb0d9447071c68b1f99a90da2cc29c89
Seth T <sethten@users.noreply.github.com>
2017-03-10 13:48:08 -0500

    More advanced WiFi example

--- WiFiInterfaceForPiStorms_examples/WiFi_Examples/program2.ino
+++ WiFiInterfaceForPiStorms_examples/WiFi_Examples/program2.ino
@@ -3,15 +3,31 @@
 #include <EVShield.h>
 #include <EVs_UIModule.h>
 
+#include <ESP8266WebServer.h>
+
+// server was already declared in program1
+// it is a global variable
+// program1 won't be running when program2 is, so we can use it here safely
+
 void setup2() {
-    // ev and uim have already been instantiated, you can just use them now.
-    ev.ledSetRGB(0, 0, 0); // ev = EVShield
-    uim.println("setup2"); // uim = UI Module
+    server.on("/", [](){
+        server.send(200, "text/html", F("<form action=\"/move\">Rotations: <br><input type=\"text\" name=\"rotations\"> <br><input type=\"submit\"></form>"));
+    });
+    server.on("/move", [](){
+        uint8_t rotations = atoi(server.arg("rotations").c_str());
+        ev.bank_a.motorRunRotations(SH_Motor_1, SH_Direction_Forward, SH_Speed_Slow,
+                                    rotations, SH_Completion_Dont_Wait, SH_Next_Action_Float);
+        server.send(200, "text/html", F("rotating... <br><a href=\"/\">back</a>"));
+    });
+    server.begin();
+    
+    uim.println("My IP address is:");
+    uim.println(WiFi.localIP());
 }
 
 void loop2() {
     // reset to browser when GO is pressed
     if (ev.getButtonState(BTN_GO)) ESP.reset();
     
-    // your creative code here
+    server.handleClient();
 }
