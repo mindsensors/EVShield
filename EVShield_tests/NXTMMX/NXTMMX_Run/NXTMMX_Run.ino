dcb9b378f1449e975826e5640b6de9c4ec85f088
mindsensors.com <info@mindsensors.com>
2021-08-10 16:21:26 -0400

    Update NXTMMX_Run.ino

--- EVShield_tests/NXTMMX/NXTMMX_Run/NXTMMX_Run.ino
+++ EVShield_tests/NXTMMX/NXTMMX_Run/NXTMMX_Run.ino
@@ -32,7 +32,10 @@ http://www.openelectrons.com/docs/viewdoc/25
 // declare the EVShield(s) attached to your Arduino.
 //
 EVShield    evshield(0x34,0x36);
-
+  byte forward = 1;
+  byte reverse = 0;
+  byte brake = 1;
+  byte float1 = 0;
 //
 // declare the i2c devices used on EVShield(s).
 //
@@ -81,11 +84,7 @@ void loop()
   byte motor2 = 2;
   byte motorboth = 3;
   byte speed = 100;
-  byte forward = 1;
-  byte reverse = 0;
   byte direction = forward;
-  byte brake = 1;
-  byte float1 = 0;
   long enc1;
   long enc2;
   int volt;
@@ -98,7 +97,7 @@ void loop()
 /**  Displays Device ID of sensor
  */
   strcpy(aa, mmx.getDeviceID() );
-  sprintf (str, "Device ID: %s", aa); Encoder value of motor 1
+  sprintf (str, "Device ID: %s", aa); //Encoder value of motor 1
   Serial.println(str);
 /**  Gets and displays Vendor ID of sensor
  */
